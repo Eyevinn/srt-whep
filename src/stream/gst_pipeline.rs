d7486cf1daa26eae306f5bc330debfe430eef06f
Kun Wu <kun.wu@eyevinn.se>
2023-09-04 16:23:59 +0200

    Fix/generate whip offer in caller mode (#52)
    * fix: avoid blocking queue and enable active-pull for fakesink

--- src/stream/gst_pipeline.rs
+++ src/stream/gst_pipeline.rs
@@ -224,27 +224,16 @@ impl PipelineBase for SharablePipeline {
             .build()?;
         let input_tee = gst::ElementFactory::make("tee").name("input_tee").build()?;
 
-        let whep_queue = gst::ElementFactory::make("queue")
-            .name("whep_queue")
-            .build()?;
+        let whep_queue = Self::create_custom_queue("whep_queue", "0", "0", "no")?;
         let typefind = gst::ElementFactory::make("typefind")
             .name("typefind")
             .build()?;
         let tsdemux = gst::ElementFactory::make("tsdemux").name("demux").build()?;
 
-        let video_queue: gst::Element = gst::ElementFactory::make("queue")
-            .name("video-queue")
-            .build()?;
-        let audio_queue: gst::Element = gst::ElementFactory::make("queue")
-            .name("audio-queue")
-            .build()?;
+        let video_queue = Self::create_custom_queue("video-queue", "0", "0", "no")?;
+        let audio_queue = Self::create_custom_queue("audio-queue", "0", "0", "no")?;
+        let srt_queue = Self::create_custom_queue("srt-queue", "0", "0", "downstream")?;
 
-        let srt_queue = gst::ElementFactory::make("queue")
-            .name("srt_queue")
-            // Drop old packets when the queue is full (so it does not block pushing thread)
-            .property_from_str("leaky", "downstream")
-            .property_from_str("max-size-buffers", "0")
-            .build()?;
         let output_uri = format!(
             "srt://{}?mode={}",
             args.output_address,
@@ -253,7 +242,6 @@ impl PipelineBase for SharablePipeline {
         tracing::info!("SRT Output uri: {}", output_uri);
         let srtsink = gst::ElementFactory::make("srtsink")
             .property("uri", output_uri)
-            .property("async", false) // to not block tee
             .property("wait-for-connection", false)
             .build()?;
 
@@ -304,17 +292,14 @@ impl PipelineBase for SharablePipeline {
                         let output_tee_video = gst::ElementFactory::make("tee")
                             .name("output_tee_video")
                             .build()?;
-                        let fakesink = gst::ElementFactory::make("fakesink").build()?;
-
-                        let mut video_elements =
-                            vec![&video_queue, &h264parse, &output_tee_video, &fakesink];
-                        // Add a fakesink to the end of pipeline if we are the listener,
+                        // Add a fakesink to the end of pipeline to consume buffers
                         // it receives and pops EOS to message bus when the SRT input stream is closed
-                        // TODO: understand why not in caller mode
-                        if srt_mode == SRTMode::Caller {
-                            video_elements.pop();
-                        }
+                        let fakesink = gst::ElementFactory::make("fakesink")
+                            .property("can-activate-pull", true)
+                            .build()?;
 
+                        let video_elements =
+                            &[&video_queue, &h264parse, &output_tee_video, &fakesink];
                         // 'video_queue' has been added to the pipeline already, so we don't add it again.
                         pipeline.add_many(&video_elements[1..])?;
                         gst::Element::link_many(&video_elements[..])?;
@@ -335,14 +320,12 @@ impl PipelineBase for SharablePipeline {
                         let output_tee_video = gst::ElementFactory::make("tee")
                             .name("output_tee_video")
                             .build()?;
-                        let fakesink = gst::ElementFactory::make("fakesink").build()?;
+                        let fakesink = gst::ElementFactory::make("fakesink")
+                            .property("can-activate-pull", true)
+                            .build()?;
 
-                        let mut video_elements =
-                            vec![&video_queue, &h265parse, &output_tee_video, &fakesink];
-                        if srt_mode == SRTMode::Caller {
-                            video_elements.pop();
-                        }
-
+                        let video_elements =
+                            &[&video_queue, &h265parse, &output_tee_video, &fakesink];
                         // 'video_queue' has been added to the pipeline already, so we don't add it again.
                         pipeline.add_many(&video_elements[1..])?;
                         gst::Element::link_many(&video_elements[..])?;
@@ -360,9 +343,11 @@ impl PipelineBase for SharablePipeline {
                         let output_tee_audio = gst::ElementFactory::make("tee")
                             .name("output_tee_audio")
                             .build()?;
-                        let fakesink = gst::ElementFactory::make("fakesink").build()?;
+                        let fakesink = gst::ElementFactory::make("fakesink")
+                            .property("can-activate-pull", true)
+                            .build()?;
 
-                        let mut audio_elements = vec![
+                        let audio_elements = &[
                             &audio_queue,
                             &aacparse,
                             &avdec_aac,
@@ -372,10 +357,6 @@ impl PipelineBase for SharablePipeline {
                             &output_tee_audio,
                             &fakesink,
                         ];
-                        if srt_mode == SRTMode::Caller {
-                            audio_elements.pop();
-                        }
-
                         // 'audio_queue' has been added to the pipeline already, so we don't add it again.
                         pipeline.add_many(&audio_elements[1..])?;
                         gst::Element::link_many(&audio_elements[..])?;
@@ -577,6 +558,32 @@ impl PipelineBase for SharablePipeline {
 
 // Helper functions
 impl SharablePipeline {
+    /// Create a queue element with given name and properties
+    /// To check if the queue is blocking, we connect to the overrun and underrun signals
+    fn create_custom_queue(
+        queue_name: &str,
+        max_size_buffers: &str,
+        max_size_time: &str,
+        leaky: &str,
+    ) -> Result<gst::Element, Error> {
+        let queue = gst::ElementFactory::make("queue")
+            .name(queue_name)
+            .property_from_str("max-size-buffers", max_size_buffers)
+            .property_from_str("max-size-time", max_size_time)
+            .property_from_str("leaky", leaky)
+            .build()?;
+
+        queue.connect("overrun", false, {
+            move |values: &[glib::Value]| {
+                let queue = values[0].get::<gst::Element>().unwrap();
+                tracing::debug!("{} is overrun", queue.name());
+                None
+            }
+        });
+
+        Ok(queue)
+    }
+
     /// Remove element from a locked pipeline
     /// # Arguments
     /// * `pipeline` - Locked pipeline
